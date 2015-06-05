164a18bd6c4c7321f1981be82b3bf2f829c6aeb0
Julien Acroute <julien-github@nura.eu>
2015-06-05 12:50:36 +0200

    Add optional argument and add interactive bpm change

--- examples/timebase_master.py
+++ examples/timebase_master.py
@@ -2,6 +2,8 @@
 
 """JACK client that set timebase master.
 
+Usage : timebase_master.py [<BPM> [<beats per bar> [<ticks per beat>]]]
+
 This Client will register itself as `timebase master` and set extended
 position information :
 
@@ -12,34 +14,39 @@ position information :
 
 """
 import jack
+import sys
 
-BPM = 130
+argv = iter(sys.argv)
+next(argv)  # skip script name
+
+BPM = next(argv, 130)
 BAR_START_TICK = 0.0
-BEATS_PER_BAR = 4.0
+BEATS_PER_BAR = float(next(argv, 4.0))
 BEAT_TYPE = 4.0
-TICKS_PER_BEAT = 960.0
-
-
-def frames2ticks(frame, ticks_per_beat, beats_per_minute, frame_rate):
-    ticks_per_second = (beats_per_minute * ticks_per_beat) / 60
-    return (ticks_per_second * frame) / frame_rate
-
+TICKS_PER_BEAT = float(next(argv, 960.0))
 
 client = jack.Client("Timebase Master")
 
+print("BPM: %s" % BPM)
+print("Beats per bar : %s" % BEATS_PER_BAR)
+print("Ticks per beat: %s" % TICKS_PER_BEAT)
+
 
 @client.set_timebase_callback
-def timebase_callback(state, nframes, pos, new_pos):
+def timebase_callback(state, blocksize, pos, new_pos):
+
+    if new_pos:
+        print("Position changed")
+
     pos.valid = 0x10  # BBT Fields
     pos.bar_start_tick = BAR_START_TICK
     pos.beats_per_bar = BEATS_PER_BAR
     pos.beat_type = BEAT_TYPE
     pos.ticks_per_beat = TICKS_PER_BEAT
     pos.beats_per_minute = BPM
-    ticks = frames2ticks(pos.frame,
-                         pos.ticks_per_beat,
-                         pos.beats_per_minute,
-                         pos.frame_rate)
+    ticks_per_second = ((pos.beats_per_minute *
+                         pos.ticks_per_beat) / 60)
+    ticks = (ticks_per_second * pos.frame) / pos.frame_rate
     (beats, pos.tick) = divmod(int(round(ticks, 0)),
                                int(round(pos.ticks_per_beat, 0)))
     (bar, beat) = divmod(beats, int(round(pos.beats_per_bar, 0)))
@@ -50,7 +57,12 @@ def timebase_callback(state, nframes, pos, new_pos):
 
 # activate !
 with client:
-    print("#" * 80)
-    print("press Return to quit")
-    print("#" * 80)
-    input()
+
+    response = BPM
+    while response != '':
+        BPM = int(response)
+        print("#" * 80)
+        print(("enter new BPM and press Return "
+               "or press Return to quit"))
+        print("#" * 80)
+        response = input()
